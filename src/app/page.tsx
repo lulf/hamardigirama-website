c1ce81d2dc524e2282ae378b0180176723cf2196
Leiv Fredrik Berge <leiv.fredrik.berge@mattilsynet.no>
2025-01-16 20:44:21 +0100

    loginlayout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>Hamar Digirama</h1>
-      </main>
+    <div className="bg-black grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Image
+        src="/Hamar_digirama_logo-01.svg"
+        height={200}
+        width={1024}
+        alt=""
+        className="m-auto max-w-[500px]"
+      />
     </div>
   );
 }
